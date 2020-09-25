b0adb0b5b63963d0e7f2ce19cf1dae39a8751dc6
beam-ntth <beam4318.t@gmail.com>
2020-09-24 17:35:33 -0700

    minor changes in meta tag

--- shared/MetaTag.js
+++ shared/MetaTag.js
@@ -4,14 +4,14 @@ const MetaTag = () => {
             <meta property="og:title" content="Dipsea Capital, LLC | We believe in providing consistent returns throughout the markets' ups and downs" />
             <meta property="og:type" content="website" />
             <meta property='og:url' content='https://www.dipseacapital.com/' />
-            <meta property='og:image' content='https://www.dipseacapital.com/DipseaLogo.png' />
+            <meta property='og:image' content='https://dipsea-capital-main-website.vercel.app/DipseaLogo.png' />
             <meta property='og:image:width' content="300" />
             <meta property='og:image:height' content="300" />
             <meta
                 property="og:description"
                 content="Dipsea Capital was established to provide its clients consistent, uncorrelated returns to major asset classes in all market environments."
             />
-            <meta name="author" content="Natheethorn 'Beam' Teacharuangchit" />
+            <meta name="author" content="Dipsea Capital, LLC Web Development Team" />
         </React.Fragment>
     );
 };
