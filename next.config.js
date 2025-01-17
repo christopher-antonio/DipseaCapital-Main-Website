d6d05ea115ffd47dd1762747823680bb85f2bb5e
Christopher Antonio <68481015+christopher-antonio@users.noreply.github.com>
2025-01-17 13:07:00 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,36 @@
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/about-us',
-          destination: '/about',
-          permanent: true,
-        },
-        {
-          source: '/meet-the-team',
-          destination: '/team',
-          permanent: true,
-        },
-        {
-          source: '/contact-us',
-          destination: '/contact',
-          permanent: true,
-        },
-        {
-          source: '/investment-philosophy',
-          destination: '/philosophy',
-          permanent: true,
-        },
-      ]
-    },
-  }
+    redirects() {
+        return [
+            process.env.MAINTENANCE_MODE === "1"
+            ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        : null,
+    ].filter(Boolean);
+    }
+};
+
+//module.exports = {
+//    async redirects() {
+//      return [
+//        {
+ //         source: '/about-us',
+//          destination: '/about',
+ //         permanent: true,
+  //      },
+   //     {
+    //      source: '/meet-the-team',
+     //     destination: '/team',
+      //    permanent: true,
+      //  },
+      //  {
+   //       source: '/contact-us',
+   //       destination: '/contact',
+   //       permanent: true,
+   //     },
+   //     {
+    //      source: '/investment-philosophy',
+    //      destination: '/philosophy',
+    //      permanent: true,
+    //    },
+   //   ]
+//    },
+//  }
